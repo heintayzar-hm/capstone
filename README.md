b703705c5920f81befd7688d2d3656b85f7e5e05
Hein Tay Zar <heintayzarhm@gmail.com>
2022-10-19 01:37:38 +0630

    Add css js

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # capstone
+
+
+
+
+
+Photo by <a href="https://unsplash.com/@munga_thigani?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Munga Thigani</a> on <a href="https://unsplash.com/s/photos/korean-people?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
+
+Photo by <a href="https://unsplash.com/es/@darranshen?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Darran Shen</a> on <a href="https://unsplash.com/s/photos/korean-people?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+
+
+Photo by <a href="https://unsplash.com/@wocintechchat?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Christina @ wocintechchat.com</a> on <a href="https://unsplash.com/s/photos/korean-people?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
+
+Photo by <a href="https://unsplash.com/@erik_lucatero?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Erik  Lucatero</a> on <a href="https://unsplash.com/s/photos/korean-people?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
+Photo by <a href="https://unsplash.com/@fotosushi?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Foto Sushi</a> on <a href="https://unsplash.com/s/photos/korean-people?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
+Photo by <a href="https://unsplash.com/ja/@freestocks?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">freestocks</a> on <a href="https://unsplash.com/s/photos/korean-people?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
+Photo by <a href="https://unsplash.com/@yvettedewit?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Yvette de Wit</a> on <a href="https://unsplash.com/s/photos/concert?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
+Photo by <a href="https://unsplash.com/@dannyhowe?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Danny Howe</a> on <a href="https://unsplash.com/s/photos/concert?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
+
+<a href="https://www.flaticon.com/free-icons/teaching" title="teaching icons">Teaching icons created by Eucalyp - Flaticon</a>
+
+<a href="https://www.flaticon.com/free-icons/exhibition" title="exhibition icons">Exhibition icons created by Freepik - Flaticon</a>
+
+<a href="https://www.flaticon.com/free-icons/group" title="group icons">Group icons created by Freepik - Flaticon</a>
+
+<a href="https://www.flaticon.com/free-icons/workshop" title="workshop icons">Workshop icons created by Flat Icons - Flaticon</a>
+
+<a href="https://www.flaticon.com/free-icons/spread" title="spread icons">Spread icons created by photo3idea_studio - Flaticon</a>
---
+++ src/index.js
@@ -0,0 +1,222 @@
+/*
+  pop-up mobile menu section
+*/
+
+const humburger = document.querySelector('#humburger');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+const mobileMenuShow = () => {
+  mobileMenu.style.display = 'block';
+};
+
+const mobileMenuHide = () => {
+  mobileMenu.style.display = 'none';
+};
+
+humburger.addEventListener('click', mobileMenuShow);
+
+const mobileMenuImg = document.querySelector('.mobile-menu-img');
+
+mobileMenuImg.addEventListener('click', mobileMenuHide);
+
+/*
+  Featured speakers section
+*/
+
+const people = [{
+  id: 1,
+  name: 'Hein Tay Zar',
+  university: 'University Of Information Technology',
+  biography: 'From the University Of Information Technology, studied computer science and lacks motivation to continue so he joined Kpop Conferrence. It is actually great.',
+  image: '../resources/people1.jpg',
+},
+{
+  id: 2,
+  name: 'Hein Tay Zar',
+  university: 'University Of Information Technology',
+  biography: 'From the University Of Information Technology, studied computer science and lacks motivation to continue so he joined Kpop Conferrence. It is actually great.',
+  image: '../resources/people2.jpg',
+},
+{
+  id: 3,
+  name: 'Hein Tay Zar',
+  university: 'University Of Information Technology',
+  biography: 'From the University Of Information Technology, studied computer science and lacks motivation to continue so he joined Kpop Conferrence. It is actually great.',
+  image: '../resources/people3.jpg',
+},
+{
+  id: 4,
+  name: 'Hein Tay Zar',
+  university: 'University Of Information Technology',
+  biography: 'From the University Of Information Technology, studied computer science and lacks motivation to continue so he joined Kpop Conferrence. It is actually great.',
+  image: '../resources/people4.jpg',
+},
+
+{
+  id: 5,
+  name: 'Hein Tay Zar',
+  university: 'University Of Information Technology',
+  biography: 'From the University Of Information Technology, studied computer science and lacks motivation to continue so he joined Kpop Conferrence. It is actually great.',
+  image: '../resources/people5.jpg',
+},
+
+{
+  id: 6,
+  name: 'Hein Tay Zar',
+  university: 'University Of Information Technology',
+  biography: 'From the University Of Information Technology, studied computer science and lacks motivation to continue so he joined Kpop Conferrence. It is actually great.',
+  image: '../resources/people6.jpg',
+},
+
+{
+  id: 7,
+  name: 'Hein Tay Zar',
+  university: 'University Of Information Technology',
+  biography: 'From the University Of Information Technology, studied computer science and lacks motivation to continue so he joined Kpop Conferrence. It is actually great.',
+  image: '../resources/people1.jpg',
+},
+];
+
+const featuredSpeakers = document.querySelector('.featured-speakers');
+// make ul tag and add class  and append it to featuredSpeakers
+const CHESSBOARD_SRC = '../resources/chessboard.svg';
+
+const peopleUl = document.createElement('ul');
+
+peopleUl.classList.add('featured-speakers-ul');
+
+featuredSpeakers.insertBefore(peopleUl, featuredSpeakers.children[2]);
+
+// generate a person and append it to featured speakers
+const generatePerson = ({
+  id, name, university, biography, image,
+}) => {
+  // make li tag and append it to ul
+  const peopleLi = document.createElement('li');
+  peopleUl.appendChild(peopleLi);
+
+  // first div
+  // make div tag and append it to li also add class
+  const firstDiv = document.createElement('div');
+  firstDiv.classList.add('full-image');
+  peopleLi.appendChild(firstDiv);
+
+  // make chessboard image and append it to div
+  const chessboard = document.createElement('img');
+  chessboard.src = CHESSBOARD_SRC;
+  chessboard.setAttribute('alt', 'chessboard');
+  chessboard.setAttribute('id', 'board');
+  firstDiv.appendChild(chessboard);
+
+  // make people image and append it to div
+  const peopleImg = document.createElement('img');
+  peopleImg.src = image.toString();
+  peopleImg.setAttribute('alt', `people${id}`);
+  peopleImg.classList.add('person-img');
+  firstDiv.appendChild(peopleImg);
+
+  // first div
+  // make div tag and append it to li
+  const secondDiv = document.createElement('div');
+  peopleLi.appendChild(secondDiv);
+
+  // make h3 tag and append it to div
+  const h3 = document.createElement('h3');
+  h3.innerText = `${name}`;
+  secondDiv.appendChild(h3);
+  // make span tag and append it to div
+  const span = document.createElement('span');
+  span.innerText = `${university}`;
+  secondDiv.appendChild(span);
+  // make p tag and append it to div
+  const p = document.createElement('p');
+  p.innerText = `${biography}`;
+  secondDiv.appendChild(p);
+};
+
+const showMore = document.querySelector('#show-more');
+
+const getInnerWidth = () => window.innerWidth;
+
+const end = document.createElement('span');
+end.innerText = 'This is the end';
+end.classList.add('hidden-if-large');
+end.setAttribute('id', 'end');
+end.style.padding = '5px';
+
+const endShow = () => {
+  end.style.display = 'block';
+};
+
+const endHide = () => {
+  end.style.display = 'none';
+};
+
+const showMoreShow = () => {
+  showMore.style.display = 'block';
+};
+
+const showMoreHide = () => {
+  showMore.style.display = 'none';
+};
+const mobilePeopleShow = () => {
+  peopleUl.innerHTML = '';
+  for (let i = 0; i < 2 && i < people.length; i += 1) {
+    generatePerson(people[i]);
+    showMoreShow();
+    endHide();
+  }
+};
+
+const desktopPeopleShow = () => {
+  peopleUl.innerHTML = '';
+  for (let i = 0; i < 6 && i < people.length; i += 1) {
+    generatePerson(people[i]);
+  }
+};
+
+const onLoadPeopleShow = () => {
+  let elementIndex = peopleUl.querySelectorAll('li').length;
+  const leftElement = people.length - peopleUl.querySelectorAll('li').length;
+  for (let i = 0; i < 4 && i < leftElement; i += 1) {
+    generatePerson(people[elementIndex]);
+    elementIndex += 1;
+  }
+};
+
+if (getInnerWidth() < 760) {
+  mobilePeopleShow();
+} else {
+  desktopPeopleShow();
+}
+
+const determineDevices = () => {
+  if (getInnerWidth() > 760) {
+    desktopPeopleShow();
+  } else {
+    mobilePeopleShow();
+  }
+};
+
+window.addEventListener('resize', determineDevices);
+
+const displayAllPeople = () => {
+  onLoadPeopleShow();
+  if (people.length === peopleUl.querySelectorAll('li').length) {
+    endShow();
+    showMoreHide();
+    featuredSpeakers.append(end);
+  }
+};
+showMore.addEventListener('click', displayAllPeople);
+
+// animation
+
+const scrollView = (link, section) => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+    section.scrollIntoView({ behavior: 'smooth' });
+  });
+};
+
+scrollView(document.querySelector('.main-program-link'), document.getElementById('main-program'));
